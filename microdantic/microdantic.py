79d8a3006b8b3a1f501186dc40a81a881242072b
christianbonnell <christian.bonnell@gmail.com>
2025-01-31 17:59:47 -0600

    feat: add xxhash implementation as utility function

--- microdantic/microdantic.py
+++ microdantic/microdantic.py
@@ -1,3 +1,60 @@
+def xxhash32(data, seed=0):
+    """
+    Optimized xxHash implementation for MicroPython.
+
+    Seee reference implementation on GitHub:
+    https://github.com/Cyan4973/xxHash
+    """
+
+    PRIME1 = 2654435761
+    PRIME2 = 2246822519
+    PRIME3 = 3266489917
+    PRIME4 = 668265263
+    PRIME5 = 374761393
+
+    def rotl32(x, r):
+        """Rotate left (circular shift) for 32-bit values."""
+        return ((x << r) & 0xFFFFFFFF) | (x >> (32 - r))
+
+    length = len(data)
+    h32 = (seed + PRIME5 + length) & 0xFFFFFFFF  # Base hash initialization
+
+    # Process 4-byte chunks
+    i = 0
+    while i + 4 <= length:
+        k1 = (
+            data[i] | (data[i + 1] << 8) | (data[i + 2] << 16) | (data[i + 3] << 24)
+        ) & 0xFFFFFFFF
+        k1 = (k1 * PRIME3) & 0xFFFFFFFF
+        k1 = rotl32(k1, 17)
+        k1 = (k1 * PRIME4) & 0xFFFFFFFF
+        h32 ^= k1
+        h32 = rotl32(h32, 19)
+        h32 = (h32 * PRIME1 + PRIME4) & 0xFFFFFFFF
+        i += 4
+
+    # Process remaining 1-3 bytes
+    if i < length:
+        if length - i == 3:
+            h32 ^= data[i + 2] << 16
+        if length - i >= 2:
+            h32 ^= data[i + 1] << 8
+        if length - i >= 1:
+            h32 ^= data[i]
+            h32 = (h32 * PRIME5) & 0xFFFFFFFF
+            h32 = rotl32(h32, 11)
+            h32 = (h32 * PRIME1) & 0xFFFFFFFF
+
+    # Final mix
+    h32 ^= h32 >> 15
+    h32 = (h32 * PRIME2) & 0xFFFFFFFF
+    h32 ^= h32 >> 13
+    h32 = (h32 * PRIME3) & 0xFFFFFFFF
+    h32 ^= h32 >> 16
+
+    return h32
+
+
 class Field:
     def __init__(
         self,
