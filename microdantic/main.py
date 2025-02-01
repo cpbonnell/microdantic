fedd07bbc598f4ddde2d025c46f60916810c2e12
christianbonnell <christian.bonnell@gmail.com>
2025-02-01 16:15:58 -0600

    test: formal tests of field validations

--- microdantic/main.py
+++ microdantic/main.py
@@ -12,7 +12,7 @@ microdantic.py to the device. The tests can be run on the host machine by
 running `poetry run tests` inside the project directory.
 """
 
-from microdantic import Field, BaseModel
+from microdantic import Field, BaseModel, Validations
 
 
 class Fruit(BaseModel):
@@ -41,8 +41,59 @@ def test_construction_and_default_values():
     assert alternate_apple.weight == 5.0
 
 
-def test_two():
-    print("...inside test_two")
+def test_validations():
+    is_even = Validations.UserSuppliedLambda(lambda x: x % 2 == 0, "Value must be even")
+
+    class ModelWithValidations(BaseModel):
+        required_even_int = Field(int, default=0, required=True, validations=[is_even])
+        optional_positive_float = Field(
+            float, default=1.0, validations=[Validations.GreaterThan(0)]
+        )
+
+    ModelWithValidations.register_class()
+
+    # Assign some values that should pass validation
+    mod = ModelWithValidations()
+
+    print("...valid assignments to required_even_int")
+    mod.required_even_int = 2
+    assert mod.required_even_int == 2
+    mod.required_even_int = -4
+    assert mod.required_even_int == -4
+
+    print("...valid assignments to optional_positive_float")
+    mod.optional_positive_float = 3.5
+    assert mod.optional_positive_float == 3.5
+    mod.optional_positive_float = 0.5
+    assert mod.optional_positive_float == 0.5
+    mod.optional_positive_float = None
+    assert mod.optional_positive_float is None
+
+    print("...error raised on assignment 'mod.required_even_int = 3.0'")
+    try:
+        mod.required_even_int = 3.0
+    except ValueError as e:
+        error_text = e.args[0]
+        assert "Failed the following validations:" in error_text
+        assert "-- Value must be of type <class 'int'>" in error_text
+        assert "-- Value must be even" in error_text
+
+    print("...error raised on assignment 'mod.required_even_int = None'")
+    try:
+        mod.required_even_int = None
+    except ValueError as e:
+        error_text = e.args[0]
+        assert "Failed the following validations:" in error_text
+        assert "-- Value must not be None" in error_text
+
+    print("...error raised on assignment 'mod.optional_positive_float = -2'")
+    try:
+        mod.optional_positive_float = -2
+    except ValueError as e:
+        error_text = e.args[0]
+        assert "Failed the following validations:" in error_text
+        assert "-- Value must be of type <class 'float'>" in error_text
+        assert "-- Value must be greater than 0" in error_text
 
 
 def run_tests():
